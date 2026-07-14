b87d944161d01b31eeb50746f5fb52583febdc60
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-07-14 18:35:39 +0530

    feat(server): implement user registration with error handling and refactor async handler

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,2 +1,23 @@
 import userRepository from "../repositories/user.repository";
+import ApiError from "../utils/ApiError";
 
+const register = async (userData) => {
+  const userExists = await userRepository.findUserByEmail(userData.email);
+
+  if (userExists) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const user = await userRepository.createUser(userData);
+
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+
+  await userRepository.updateRefreshToken(user._id, refreshToken);
+
+  return {
+    user,
+    accessToken,
+    refreshToken,
+  };
+};
