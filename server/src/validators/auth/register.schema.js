71cab0c913bf8e414bc513dc045061c8d78fe96a
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-07-15 16:44:43 +0530

    feat(server): add password validation to registration schema

--- server/src/validators/auth/register.schema.js
+++ server/src/validators/auth/register.schema.js
@@ -1,9 +1,16 @@
-import { email, z } from "zod";
+import { z } from "zod";
+import { PASSWORD_REGEX } from "../../constants/regex";
 
 const registerSchema = z.object({
   name: z.string().min(2).max(50).trim(),
   email: z.string().trim().toLowerCase().email(),
-  password: z.string(),
+  password: z
+    .string()
+    .min(8)
+    .regex(
+      PASSWORD_REGEX,
+      "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character.",
+    ),
 });
 
 export default registerSchema;
