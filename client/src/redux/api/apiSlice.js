e68c8f4c13ff8a998fb47d4c2ccc88067de8725e
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-09-01 19:46:32 +0530

    feat(client): implement reauthentication logic in API slice

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -5,10 +5,31 @@ const baseQuery = fetchBaseQuery({
   credentials: "include",
 });
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    const refreshResult = await baseQuery(
+      {
+        url: "/api/v1/auth/refresh-token",
+        method: "POST",
+      },
+      api,
+      extraOptions,
+    );
+
+    if (!refreshResult.error) {
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
   reducerPath: "api",
 
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
 
   tagTypes: [
     "User",
