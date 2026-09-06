f755d8ffbf116b6785f6fe25ee0b737df3bc1fc3
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-09-06 17:35:22 +0530

    feat: update cookie options to set sameSite attribute to 'none' for improved cross-site cookie handling

--- server/src/constants/cookieOptions.js
+++ server/src/constants/cookieOptions.js
@@ -1,16 +1,17 @@
 import ms from "ms";
+
 import env from "../config/env.js";
 
 export const accessTokenCookieOptions = {
   httpOnly: true,
   secure: env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: ms(env.ACCESS_TOKEN_EXPIRES_IN),
 };
 
 export const refreshTokenCookieOptions = {
   httpOnly: true,
   secure: env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: ms(env.REFRESH_TOKEN_EXPIRES_IN),
 };
